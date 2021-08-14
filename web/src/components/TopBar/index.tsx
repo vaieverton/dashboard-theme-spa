756143cfe02195bb1e095bc2d77592f3cf992ecf
vaieverton <everton.scorpion@hotmail.com>
2021-08-14 19:32:10 -0400

    correct styles in app [WEB]

--- web/src/components/TopBar/index.tsx
+++ web/src/components/TopBar/index.tsx
@@ -4,21 +4,22 @@ import { useTheme } from '../../context/themeContext';
 import './styles.scss';
 
 const TopBar: React.FC = () => {
-    const {isToggled, changeToggle} = useTheme();
-  return (
-      <div className="main-topbar">
-          <div>
-              <button type="button" onClick={changeToggle}>
-              {isToggled ? '<' : '>'}
-              </button>
-              
-          </div>
+    const { isToggled, changeToggle, isDark } = useTheme();
 
-          <div>
-              content right
-          </div>
-      </div>
-  );
+    return (
+        <div className="main-topbar" style={isDark ? {background: "#1a1a1a", transition: '0.5s'} : {background: "#acacac"}}>
+            <div>
+                <button type="button" onClick={changeToggle}>
+                    {isToggled ? '<' : '>'}
+                </button>
+
+            </div>
+
+            <div>
+                content right
+            </div>
+        </div>
+    );
 }
 
 export default TopBar;
